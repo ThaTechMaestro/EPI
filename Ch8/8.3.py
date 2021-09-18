b05cc077370c74425f0ca8e243ef6bf696706845
ThaTechMaestro <muse.pearls@gmail.com>
2021-09-18 20:56:52 +0100

    Adds explanatory comment to 8.3

--- Ch8/8.3.py
+++ Ch8/8.3.py
@@ -46,6 +46,7 @@ else:
 
 '''
 
+#https://leetcode.com/problems/valid-parentheses/discuss/?currentPage=1&orderBy=hot&query=
 
 
 #------------------------------
@@ -54,46 +55,55 @@ def is_well_formed(s):
 
   open_symb_store = []
 
-  lookup = {'(':')', '{':'}', '[':']'}
+  symbol_lookup_table = {'(':')', '{':'}', '[':']'}
 
   for symbol in s:
-    if symbol in lookup:
+    if symbol in symbol_lookup_table:
       open_symb_store.append(symbol)
-    elif not open_symb_store or lookup[open_symb_store.pop()] != symbol:
+    elif not open_symb_store or symbol_lookup_table[open_symb_store.pop()] != symbol:
       return False 
   
   return not open_symb_store
 
 #-----------------------------------------
 # Style 2
+# What important thing did you learn from this style
+#   that will help your problem solving skills moving forward
+
 def is_well_formed(s):
 
   open_symb_store = []
 
   lookup_table = {'(':')', '{':'}', '[':']'}
 
+  #Loop through each symbol entered
   for symbol in s:
+    # if symbol is an open parenthesis, since we loop through the keys in a dictionary
     if symbol in lookup_table:
+
+      #Store the open parentheis
       open_symb_store.append(symbol)
+
+    # Based on our open symbol store
+    # We check if the recently added open symbolto our store has a matching closing symbol
+    # This is where the stack comes in
+    # Now incases of nested opening parenthesis (edge case)
+    #   The corresponding closing symbol will be based on the last inserted parenthesis
     elif lookup_table[open_symb_store.pop()] != symbol:
       return False
+    
+    # A not statement means the false condition evaluates to true
+    # In our case, if we have an empty open symbol store (edge case)
+    # This works for inputs such as "))))))"
     elif not open_symb_store:
       return False 
   
+  # In cases where we have an empty string (edge case)
+  #   A nice question will be what should we return when we have an empty input?
+  # We return True statement, meaning we have a valid parentheis
   return not open_symb_Store
 
 #-------------------------------------------------
-#Style 3
-def is_well_formed(s):
-
-  open_symbol_store = []
-  lookup_table = {'(':')', '{':'}', '[':']'}
-
-  for symbol in s:
-
-    if symbol in lookup_table:
-      open_symbol_store.append(symbol)
-    elif symbol == lookup_table[]
 
 
 
