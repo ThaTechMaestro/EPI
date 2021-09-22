cc3a0d06ac506c7ecced48c970d1e4a3f56df27d
ThaTechMaestro <muse.pearls@gmail.com>
2021-09-22 16:43:00 +0100

    Re-implementing new solution to 8.3

--- Ch8/test.py
+++ Ch8/test.py
@@ -1,3 +1,6 @@
+# https://leetcode.com/problems/valid-parentheses/discuss/1407199/EASY-SOLUTION-or-PYTHON-or-FATER-THAN-90-or-please-upvote
+# Checking a new solution
+
 def test_valid_parenthesis(s):
 
 
