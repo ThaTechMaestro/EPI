130f1c584b6e210b5daf71c5ffa1b78031a96dab
ThaTechMaestro <muse.pearls@gmail.com>
2021-09-26 21:19:04 +0100

    Progress comment for tommorow

--- Ch8/test.py
+++ Ch8/test.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/valid-parentheses/discuss/1407199/EASY-SOLUTION-or-PYTHON-or-FATER-THAN-90-or-please-upvote
 # Checking a new solution
+# I will continue from here tommorow morning
 
 def test_valid_parenthesis(s):
 
